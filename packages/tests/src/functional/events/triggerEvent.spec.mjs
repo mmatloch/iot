862f75586657a22a1f44b0afddf242c2d46bbedc
Mateusz Matloch <mateusz.matloch98@gmail.com>
2022-07-08 19:45:46 +0000

    add circular reference test

--- packages/tests/src/functional/events/triggerEvent.spec.mjs
+++ packages/tests/src/functional/events/triggerEvent.spec.mjs
@@ -173,6 +173,100 @@ describe('Events triggerEvent', () => {
             expectPerformanceMetrics(thirdProcessedEvent);
         });
 
+        describe('circular reference', () => {
+            it('should detect a circular reference in the same event chain', async () => {
+                // given
+                const firstEventPayload = generateEventPostPayload();
+                const { body: firstEvent } = await eventHelpers.post(firstEventPayload).expectSuccess();
+
+                const secondEventPayload = generateEventPostPayload();
+                secondEventPayload.actionDefinition = `
+                    const filters = {_id: ${firstEvent._id}};
+                    await sdk.eventRunner.trigger({filters, context});
+                `;
+
+                const { body: secondEvent } = await eventHelpers.post(secondEventPayload).expectSuccess();
+
+                const eventUpdatePayload = {
+                    actionDefinition: `
+                        const filters = {_id: ${secondEvent._id}};
+                        await sdk.eventRunner.trigger({filters, context});
+                    `,
+                };
+
+                await eventHelpers.patchById(firstEvent._id, eventUpdatePayload).expectSuccess();
+
+                const payload = generateEventTriggerPayload();
+                payload.filters.triggerType = secondEvent.triggerType;
+                payload.filters.triggerFilters = secondEvent.triggerFilters;
+
+                // when
+                const { body: triggerResult } = await H.post(payload).expectSuccess();
+
+                // then
+                expect(triggerResult.processedEvents).toBeArrayOfSize(1);
+
+                const [firstProcessedEvent] = triggerResult.processedEvents;
+                expect(firstProcessedEvent.eventInstance).toMatchObject({
+                    eventId: secondEvent._id,
+                    state: 'SUCCESS',
+                });
+                expect(firstProcessedEvent.processedEvents).toBeArrayOfSize(1);
+
+                const [secondProcessedEvent] = firstProcessedEvent.processedEvents;
+                expect(secondProcessedEvent.eventInstance).toHaveProperty('state', 'FAILED_ON_ACTION');
+                expect(secondProcessedEvent.eventInstance).toHaveProperty('error');
+                expect(secondProcessedEvent.eventInstance.error).toMatchObject({
+                    message: 'Error occurred during the event action',
+                    errorCode: 'SRV-9',
+                });
+                expect(secondProcessedEvent.eventInstance.error.cause).toMatchObject({
+                    detail: `Detected that event ${secondEvent._id}, triggered by event ${firstEvent._id} is trying to run again in the same event chain. This is regarded as undesirable because it can lead to an infinite loop`,
+                    message: 'Circular reference in the event trigger chain',
+                    errorCode: 'SRV-11',
+                });
+            });
+
+            it('should detect a circular reference when the event tries to trigger itself', async () => {
+                // given
+                const eventPayload = generateEventPostPayload();
+                const { body: event } = await eventHelpers.post(eventPayload).expectSuccess();
+
+                const eventUpdatePayload = {
+                    actionDefinition: `
+                        const filters = {_id: ${event._id}};
+                        await sdk.eventRunner.trigger({filters, context});
+                    `,
+                };
+
+                await eventHelpers.patchById(event._id, eventUpdatePayload).expectSuccess();
+
+                const payload = generateEventTriggerPayload();
+                payload.filters.triggerType = event.triggerType;
+                payload.filters.triggerFilters = event.triggerFilters;
+
+                // when
+                const { body: triggerResult } = await H.post(payload).expectSuccess();
+
+                // then
+                expect(triggerResult.processedEvents).toBeArrayOfSize(1);
+
+                const [processedEvent] = triggerResult.processedEvents;
+
+                expect(processedEvent.eventInstance).toHaveProperty('state', 'FAILED_ON_ACTION');
+                expect(processedEvent.eventInstance).toHaveProperty('error');
+                expect(processedEvent.eventInstance.error).toMatchObject({
+                    message: 'Error occurred during the event action',
+                    errorCode: 'SRV-9',
+                });
+                expect(processedEvent.eventInstance.error.cause).toMatchObject({
+                    detail: `Detected that event ${event._id}, triggered by event ${event._id} is trying to run again in the same event chain. This is regarded as undesirable because it can lead to an infinite loop`,
+                    message: 'Circular reference in the event trigger chain',
+                    errorCode: 'SRV-11',
+                });
+            });
+        });
+
         it('should return an empty result when there is no event matching the filters', async () => {
             // given
             const payload = generateEventTriggerPayload();
