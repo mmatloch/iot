ea4d8bd54898b35d26b4793d98957434893d0564
Mateusz Matloch <mateusz.matloch98@gmail.com>
2022-07-23 21:46:33 +0000

    add yarn install to build script

--- packages/scripts/src/commands/build.ts
+++ packages/scripts/src/commands/build.ts
@@ -60,6 +60,8 @@ export class BuildCommand extends Command {
     async run() {
         const { flags } = await this.parse<Flags, Record<string, unknown>>(BuildCommand);
 
+        await x('yarn install');
+
         for (const { name, buildCondition, buildArgs, dockerfilePath, imageName, imageTag } of createDockerImages(
             flags,
         )) {
