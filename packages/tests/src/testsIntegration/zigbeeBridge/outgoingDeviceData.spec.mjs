0e379117c3f7890fe4dd0b64d903c5dace1f229d
Mateusz Matloch <mateusz.matloch98@gmail.com>
2022-11-06 12:19:57 +0000

    debug

--- packages/tests/src/testsIntegration/zigbeeBridge/outgoingDeviceData.spec.mjs
+++ packages/tests/src/testsIntegration/zigbeeBridge/outgoingDeviceData.spec.mjs
@@ -78,7 +78,7 @@ describe('Zigbee bridge outgoingDeviceData', () => {
         await disconnectFromBroker();
     });
 
-    it.only('should publish data', async () => {
+    it('should publish data', async () => {
         // given
         const topic = getZigbeeTopic(device).toPublishData;
         const receivedMessages = await H.subscribe(topic);
