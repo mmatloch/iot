e30cab62fdc72a16dcff3426f2818b46975ecdd7
mmatloch <66954771+mmatloch@users.noreply.github.com>
2023-05-05 20:34:28 +0200

    fix(frontend): interval field is not sent to the API (#31)

--- packages/frontend/src/utils/modifyEventDto.ts
+++ packages/frontend/src/utils/modifyEventDto.ts
@@ -1,14 +1,14 @@
-import type { EventDto} from '@definitions/entities/eventTypes';
+import type { EventDto } from '@definitions/entities/eventTypes';
 import { EventMetadataTaskType, EventTriggerType } from '@definitions/entities/eventTypes';
 import { cloneDeep } from 'lodash';
 
-const isStatic = (dto: EventDto) =>
+const isRelative = (dto: EventDto) =>
     dto.metadata?.taskType === EventMetadataTaskType.RelativeCron ||
     dto.metadata?.taskType === EventMetadataTaskType.RelativeInterval;
 
-const isRelative = (dto: EventDto) =>
-    dto.metadata?.taskType === EventMetadataTaskType.StaticCron ||
-    dto.metadata?.taskType === EventMetadataTaskType.StaticInterval;
+const isInterval = (dto: EventDto) =>
+    dto.metadata?.taskType === EventMetadataTaskType.StaticInterval ||
+    dto.metadata?.taskType === EventMetadataTaskType.RelativeInterval;
 
 const isCron = (dto: EventDto) =>
     dto.metadata?.taskType === EventMetadataTaskType.StaticCron ||
@@ -22,14 +22,14 @@ export const prepareEventDto = (orginalDto: EventDto): EventDto => {
     }
 
     if (dto.metadata) {
-        if (isRelative(dto)) {
-            // @ts-expect-error this field doesn't exist in the interface
-            delete dto.metadata.runAfterEvent;
-        }
-
-        if (isStatic(dto)) {
+        if (!isInterval(dto)) {
             // @ts-expect-error this field doesn't exist in the interface
             delete dto.metadata.interval;
+        }
+
+        if (!isRelative(dto)) {
+            // @ts-expect-error this field doesn't exist in the interface
+            delete dto.metadata.runAfterEvent;
         }
 
         if (!isCron(dto)) {
