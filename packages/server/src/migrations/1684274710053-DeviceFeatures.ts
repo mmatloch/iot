c5080b074cf8a080cac67a7486e0cd84ac6d716f
Mateusz Matloch <mateusz.matloch98@gmail.com>
2023-05-28 20:14:48 +0000

    fix(server): device features migration

--- packages/server/src/migrations/1684274710053-DeviceFeatures.ts
+++ packages/server/src/migrations/1684274710053-DeviceFeatures.ts
@@ -1,16 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class DeviceFeatures1684274710053 implements MigrationInterface {
-    name = 'DeviceFeatures1684274710053'
+    name = 'DeviceFeatures1684274710053';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "devices" ADD "features" jsonb NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "devices" ADD "featureState" jsonb NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "devices" ADD "features" jsonb NOT NULL DEFAULT '[]'::jsonb`);
+        await queryRunner.query(`ALTER TABLE "devices" ADD "featureState" jsonb NOT NULL DEFAULT '{}'::jsonb`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "devices" DROP COLUMN "featureState"`);
         await queryRunner.query(`ALTER TABLE "devices" DROP COLUMN "features"`);
     }
-
 }
